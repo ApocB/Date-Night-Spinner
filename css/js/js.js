e1edf9856bbe5b333c0ced839dca55ccebc9eadb
Michael Buck <107284632+ApocB@users.noreply.github.com>
2025-08-20 13:13:11 +0000

    rework

--- css/js/js.js
+++
@@ -1,7 +0,0 @@
-const text = document.createElementNS(svgNS, "text");
-text.setAttribute("dy", "-5");
-text.setAttribute("fill", "white");
-text.setAttribute("font-size", "16");         // 🔼 bigger font
-text.setAttribute("font-weight", "bold");
-text.setAttribute("stroke", "black");         // 🔼 outline for readability
-text.setAttribute("stroke-width", "0.8");     // thin border around letters
